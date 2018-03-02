c35fc01577dcbb9a79ed1206b60f80eb9730a628
Anish Adhikari <anish531213@gmail.com>
2018-03-02 11:08:34 -0500

    Read all the binary data

--- practice.c
+++ practice.c
@@ -6,7 +6,7 @@
 int main()
 
 {
-	unsigned char buffer[30];
+	unsigned char buffer[1];
 
 	int count;
 	FILE *ptr;
@@ -18,10 +18,24 @@ int main()
 		return 1;
 	}
 
+	while (fread(&buffer,sizeof(buffer),1,ptr) != 0) {
+		printf("%u ", buffer[0]);
+	}
+
+	printf("\n");
 	// for (count=1; count <= 10; count++) { 
-	fread(&buffer,sizeof(buffer),1,ptr);   
+	/*
+	fread(&buffer,sizeof(buffer),1,ptr);
+	int i = 0;  
+
+	while (buffer[i] != '\0') {
+		printf("%u ", buffer[i]);	
+		i+= 1;
+	}
+
 	for(int i = 0; i<30; i++)
-		printf("%u ", buffer[i]);	 
+		printf("%u ", buffer[i]);
+	*/	 
 
 	fclose(ptr);
 	return 0;
